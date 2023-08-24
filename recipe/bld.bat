1f72c94bcb4a885f19b08816329d78ea06897525
Bradley Kreider <1699478+bkreider@users.noreply.github.com>
2023-08-24 15:34:30 -0400

    Updated json shortcut filename and bumped version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,5 +4,5 @@ IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 copy %RECIPE_DIR%\Iconleak-Atrous-PSConsole.ico %MENU_DIR%
 if errorlevel 1 exit 1
 
-copy %RECIPE_DIR%\menu-windows-ps.json %MENU_DIR%\powershell_shortcut.json
+copy %RECIPE_DIR%\menu-windows-ps.json %MENU_DIR%\powershell_shortcut_anacondapro.json
 if errorlevel 1 exit 1
